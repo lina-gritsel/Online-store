adc862bab25fe746f9e682dbcb13c9f01d75d0bc
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2022-12-23 01:51:42 +0300

    feat: add ordar modal validate

--- src/components/CreateOrderModal/CreateOrderModal.ts
+++ src/components/CreateOrderModal/CreateOrderModal.ts
@@ -1,11 +1,18 @@
 import { getProduct } from '../../api'
 import { parseRequestURL } from '../../utils'
 import styles from './CreateOrderModal.module.scss'
-import masterCard from '../../assets/svg/masterCard.svg'
-import visa from '../../assets/svg/visa.svg'
-import express from '../../assets/svg/american.svg'
-import discover from '../../assets/svg/discover.svg'
 
+import {
+  checkIsUserNameValid,
+  checkIsPhoneNumberValid,
+  checkIsAddressValid,
+  checkIsEmailValid,
+  checkIsCardNumberValid,
+  checkIsCvvValid,
+} from './modules/fieldsValidate'
+import { checkCardTerm } from './modules/cardTerm'
+import { cardNumberSeparation } from './modules/helpers'
+import { setCardImage } from './modules/setCardImage'
 
 export default {
   render: async () => {
@@ -21,11 +28,11 @@ export default {
               <span>Error message</span>
             </div>
             <div class='${styles.wrapperInput}'>
-              <input type=text id=phone class='${styles.formInput} input' placeholder='Phone number'>
+              <input type="number" id=phone class='${styles.formInput} input' placeholder='Phone number'>
               <span>Error message</span>
             </div>
             <div class=${styles.wrapperInput}>
-              <input type=text id='address' class='${styles.formInput} input' placeholder='Delivery address'>
+              <input type="text" id='address' class='${styles.formInput} input' placeholder='Delivery address'>
               <span>Error message</span>
             </div>
             <div class=${styles.wrapperInput}>
@@ -40,7 +47,12 @@ export default {
                 <img class=${styles.cardLogo} id=cardLogo src='' />
               </div>
               <div class=${styles.wrapperInput}>
-                <input type="number" class=${styles.cardNumberInput} id=cardNumber placeholder='**** **** **** ****' oninput="validity.valid||(value='')" onKeyDown="if(this.value.length==16 && event.keyCode>47 && event.keyCode < 58) return false;">
+                <input type="text"
+                 onkeypress="if(this.value.length==19 && event.keyCode>47 && event.keyCode < 58) return false; return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))"
+                  class=${styles.cardNumberInput}
+                  id=cardNumber
+                  placeholder='**** **** **** ****'
+                <span><span>
               </div>
               <div class=${styles.cardInfo}>
                 <div class=${styles.expWrapper}>
@@ -91,191 +103,40 @@ export default {
     const address = document.getElementById('address') as HTMLInputElement
     const email = document.getElementById('email') as HTMLInputElement
     const cardNumber = document.getElementById('cardNumber') as HTMLInputElement
-    const monthInput = document.getElementById('month') as HTMLInputElement
-    const yearInput = document.getElementById('year') as HTMLInputElement
+
     const cvv = document.getElementById('cvv') as HTMLInputElement
-    const cardLogo = document.getElementById('cardLogo') as HTMLImageElement
 
-    form?.addEventListener('submit', (e) => {
+    form?.addEventListener('submit', (e: SubmitEvent) => {
       e.preventDefault()
-
-      const cvvValue = cvv.value.trim()
-      console.log(cvvValue.length)
-
       checkInputs()
     })
 
-    const checkInputs = () => {
+    const checkInputs = (): void => {
       const userNameValue = userName.value.trim()
       const phoneValue = phone.value.trim()
       const addressValue = address.value.trim()
       const emailValue = email.value.trim()
-      const cvvValue = cvv.value.trim()
       const cardNumberValue = cardNumber.value.trim()
+      const cvvValue = cvv.value.trim()
 
-      const nameArray = userNameValue.split(' ')
-      const nameIsString = nameArray.map((namePart) =>
-        /^[a-zA-Z]+$/.test(namePart),
-      )
-      const nameParts = userNameValue.split(' ')
-      const validNameArray = nameParts.map((namePart) => namePart.length < 3)
-      const addressArray = addressValue.split(' ')
-      const validAddressArray = addressArray.map(
-        (adressPart) => adressPart.length < 5,
-      )
-      const addressIsString = addressArray.map((addressPart) =>
-        /^[a-zA-Z]+$/.test(addressPart),
-      )
-
-      if (userNameValue === '') {
-        setErrorFor(userName, 'User name cannot be blank')
-      } else if (nameIsString.includes(false)) {
-        setErrorFor(userName, 'Enter the string')
-      } else if (nameParts.length < 2) {
-        setErrorFor(userName, 'Required number of words - 2')
-      } else if (validNameArray.includes(true)) {
-        setErrorFor(userName, 'Name or surname length of at least 3 characters')
-      } else {
-        setSuccessFor(userName)
-      }
-
-      if (phoneValue === '') {
-        setErrorFor(phone, 'Phone number cannot be blank')
-      } else if (!/^\d+$/.test(phoneValue.slice(1))) {
-        setErrorFor(phone, 'Expected value of a digit')
-      } else if (phoneValue.slice(0, 1) !== '+') {
-        setErrorFor(phone, 'Required "+" at the beginning')
-      } else if (phoneValue.length < 9) {
-        setErrorFor(phone, 'Minimum length 9')
-      } else {
-        setSuccessFor(phone)
-      }
-
-      if (addressValue === '') {
-        setErrorFor(address, 'Address cannot be blank')
-      } else if (addressIsString.includes(false)) {
-        setErrorFor(address, 'Enter the string')
-      } else if (addressArray.length < 3) {
-        setErrorFor(address, 'Too short address')
-      } else if (validAddressArray.includes(true)) {
-        setErrorFor(address, 'Insufficient number of characters')
-      } else {
-        setSuccessFor(address)
-      }
-
-      if (emailValue === '') {
-        setErrorFor(email, 'Email name cannot be blank')
-      } else if (!isEmail(emailValue)) {
-        setErrorFor(email, 'Email is not valid')
-      } else {
-        setSuccessFor(email)
-      }
-
-      if (cvvValue === '') {
-        setErrorFor(cvv, 'CVV cannot be blank')
-      } else {
-        setSuccessFor(cvv)
-      }
-
-      if (cardNumberValue === '') {
-        setErrorFor(cardNumber, 'Card Number cannot be blank')
-      } else if (cardNumberValue.length !== 16) {
-        setErrorFor(cardNumber, 'Card length 16 digits')
-      } else {
-        setSuccessFor(cardNumber)
-      }
+      checkIsUserNameValid(userNameValue)
+      checkIsPhoneNumberValid(phoneValue)
+      checkIsAddressValid(addressValue)
+      checkIsEmailValid(emailValue)
+      checkIsCardNumberValid(cardNumberValue)
+      checkIsCvvValid(cvvValue)
     }
 
-    const setErrorFor = (input: HTMLInputElement, message: string) => {
-      const formControl = input.parentElement as HTMLElement
-      const span = formControl?.querySelector('span') as HTMLSpanElement
-      span.innerText = message
-      formControl.className = `${styles.error}`
-    }
+    checkCardTerm()
 
-    const setSuccessFor = (input: HTMLInputElement) => {
-      const formControl = input.parentElement as HTMLElement
-      formControl.className = `${styles.success}`
-    }
+    cardNumber.addEventListener('keyup', (e: KeyboardEvent) => {
+      const splittedCardNumber = cardNumberSeparation(cardNumber.value)
+      const firstValueNumber = cardNumber.value.slice(0, 1)
 
-    const isEmail = (emailValue: string) => {
-      return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        emailValue,
-      )
-    }
+      const targetValue = e.target as HTMLInputElement
+      targetValue.value = splittedCardNumber
 
-    cardNumber.addEventListener('keyup', () => {
-      if (cardNumber.value.slice(0, 1) === '6') {
-        cardLogo.src = discover
-      } else if (cardNumber.value.slice(0, 1) === '5') {
-        cardLogo.src = masterCard
-      } else if (cardNumber.value.slice(0, 1) === '4') {
-        cardLogo.src = visa
-      } else if (cardNumber.value.slice(0, 1) === '3') {
-        cardLogo.src = express
-      } else if (cardNumber.value === '') {
-        cardLogo.src = ''
-      }
+      setCardImage(firstValueNumber)
     })
-
-    const focusSibling = function (
-      target: any,
-      direction: any,
-      callback?: any,
-    ) {
-      const nextTarget = target[direction]
-      nextTarget && nextTarget.focus()
-      // if callback is supplied we return the sibling target which has focus
-      callback && callback(nextTarget)
-    }
-
-    monthInput?.addEventListener('input', (event: Event) => {
-      const value = (event.target as HTMLInputElement).value.toString()
-      if (value.length === 1 && +value > 1) {
-        ;(event.target as HTMLInputElement).value = '0' + value
-      }
-      if (value === '00') {
-        ;(event.target as HTMLInputElement).value = '01'
-      } else if (+value > 12) {
-        ;(event.target as HTMLInputElement).value = '12'
-      }
-      2 <= (event.target as HTMLInputElement).value.length &&
-        focusSibling(event.target, 'nextElementSibling')
-      event.stopImmediatePropagation()
-    })
-
-    yearInput.addEventListener('keydown', (event) => {
-      if (
-        event.key === 'Backspace' &&
-        (event.target as HTMLInputElement).selectionStart === 0
-      ) {
-        focusSibling(event.target, 'previousElementSibling')
-        event.stopImmediatePropagation()
-      }
-    })
-
-    const inputMatchesPattern = function (e: any) {
-      const { value, selectionStart, selectionEnd, pattern } = e.target
-
-      const character = String.fromCharCode(e.which)
-      const proposedEntry =
-        value.slice(0, selectionStart) + character + value.slice(selectionEnd)
-      const match = proposedEntry.match(pattern)
-
-      return (
-        e.metaKey || // cmd/ctrl
-        e.which <= 0 || // arrow keys
-        e.which == 8 || // delete key
-        (match && match['0'] === match.input)
-      ) // pattern regex isMatch - workaround for passing [0-9]* into RegExp
-    }
-
-    document.querySelectorAll('input[data-pattern-validate]').forEach((el) =>
-      el.addEventListener('keypress', (e) => {
-        if (!inputMatchesPattern(e)) {
-          return e.preventDefault()
-        }
-      }),
-    )
   },
 }
