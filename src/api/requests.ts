e2596012c93cc4edc1a0dbf1b18c3d2e65b74c46
JDM <shchuka_pasha@mail.ru>
2023-01-08 21:04:27 +0300

    refactor: refactor some code

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -2,15 +2,16 @@ import { Products } from './types'
 
 const baseUrl = 'https://strange-shawl-mite.cyclic.app'
 
-export const getAllProducts = async (): Promise<Products[]> => {
+export const getAllProducts = async <T extends Products[]>(): Promise<T> => {
   const result = await fetch(`${baseUrl}/products`)
   const { products } = await result.json()
-
-  return products as Products[]
+  return products
 }
 
-export const getProduct = async (id: string): Promise<Products> => {
+export const getProduct = async <T extends Products>(
+  id: string,
+): Promise<T> => {
   const result = await fetch(`${baseUrl}/products/${id}`)
-  const  { product }  = await result.json()
-  return product as Products
+  const { product } = await result.json()
+  return product
 }
